4a34e87fbd04cf66c436962cf0bff3845c5dab37
Rafoolin <niloo.jv@gmail.com>
2024-11-01 21:08:28 +0100

    feat: create a package out of the project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    author="Rafoolin",
+    author_email="niloo.jv@gmail.com",
+    description="DSSS Homework 02!",
+    long_description="A Math quiz for FAU DSSS WS 2024/2025",
+    url="https://github.com/rafoolin/dsss_ws24_homework_02",
+    packages=find_packages(),
+    install_requires=[],
+)
